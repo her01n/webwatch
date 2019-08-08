303b0d599b764ac1a1680bdad38c47a3f17e570c
Michal Herko <misko.herko@gmail.com>
2019-08-08 21:02:58 +0200

    Feature: Log

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ export WEBWATCH_DEST=$(realpath test/)
 echo
 echo "Test test pass"
 rm -rf test/status
+echo "prev logs" > test/log
 env WEBWATCH_CONFIG=$(realpath test/pass/) ./webwatch
 if [ ! -f test/status ]; then
     echo "Status file not generated"
@@ -15,10 +16,18 @@ if ! grep -i "ok" test/status ; then
     echo "ok status should be reported"
     false
 fi
+if ! grep "prev logs" test/log ; then
+    echo "old logs were removed"
+    false
+fi
+if ! grep "google.com" test/log | grep -i "success" ; then
+    echo "success were not logged"
+    false
+fi
 
 echo
 echo "Test failure connection refused"
-rm -rf test/status
+rm -rf test/status test/log
 env WEBWATCH_CONFIG=$(realpath test/failure/) ./webwatch
 if [ ! -f test/status ]; then
     echo "Status file not generated"
@@ -34,6 +43,10 @@ if ! grep -i "fail" test/status ; then
 fi
 if ! grep -i "refused" test/status; then
     echo "connection refused should be reported"
+    false
+fi
+if ! grep "asdf" test/log | grep -i "fail" ; then
+    echo "failure were not logged"
     false
 fi
 
