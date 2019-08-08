b8736c313f80a19c64fc532d26857ac980684518
Michal Herko <misko.herko@gmail.com>
2019-08-08 20:27:30 +0200

    Bug: Server Errors should be reported

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ if ! grep -i "ok" test/status ; then
 fi
 
 echo
-echo "Test test failure"
+echo "Test failure connection refused"
 rm -rf test/status
 env WEBWATCH_CONFIG=$(realpath test/failure/) ./webwatch
 if [ ! -f test/status ]; then
@@ -32,8 +32,29 @@ if ! grep -i "fail" test/status ; then
     echo "failure status not reported"
     false
 fi
-if ! grep -i "not resolve" test/status; then
-    echo "name not resolved should be reported"
+if ! grep -i "refused" test/status; then
+    echo "connection refused should be reported"
+    false
+fi
+
+echo
+echo "Test failure server responds 404"
+rm -rf test/status
+env WEBWATCH_CONFIG=$(realpath test/errorcode/) ./webwatch
+if [ ! -f test/status ]; then
+    echo "Status file not generated"
+    false
+fi
+if grep -i "ok" test/status ; then
+    echo "ok status reported"
+    false
+fi
+if ! grep -i "fail" test/status ; then
+    echo "failure status not reported"
+    false
+fi
+if ! grep -i "404" test/status; then
+    echo "error code 404 should be reported"
     false
 fi
 
